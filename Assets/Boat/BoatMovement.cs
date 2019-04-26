3b4612eca564324f0cea6a6dabeccd1a90ecf1fb
Smitchy <life.lime@gmail.com>
2019-04-26 23:18:11 +0200

    simple Movement

---
+++ Assets/Boat/BoatMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatMovement : MonoBehaviour
+{
+
+    public float acceleration;
+    public float rotation;
+    public float maxSpeed;
+
+    private Rigidbody2D rBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, 0, rotation));
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, 0, -rotation));
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rBody.AddForce(transform.up * acceleration);   
+        }
+
+        if(rBody.velocity.magnitude > maxSpeed)
+        {
+            rBody.velocity = rBody.velocity.normalized * maxSpeed;
+        }
+
+    }
+    
+}
