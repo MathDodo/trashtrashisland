4eb85dcd0f6207a1b1679f94116a9e108cd3a780
MathDodo <mathiasdodo@hotmail.com>
2019-04-27 21:16:57 +0200

    - sludge

---
+++ Assets/Grid/TriggerFade.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerFade : MonoBehaviour
+{
+    [SerializeField]
+    private float _fadeTime = 1.5f;
+
+    public bool _Fade = false;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (_Fade)
+        {
+            var color = _spriteRenderer.color;
+            color.a -= Time.deltaTime / _fadeTime;
+            _spriteRenderer.color = color;
+
+            if (color.a <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Untagged"))
+        {
+            _Fade = true;
+        }
+    }
+}
