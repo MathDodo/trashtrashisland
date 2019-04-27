8e3a709b7ebbb86dba9964d8ca63f69582e8a8c8
Peter Witt <peter.witt.joergensen@gmail.com>
2019-04-27 19:44:16 +0200

    Finished main menu

---
+++ Assets/UI/SpriteMover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMover : MonoBehaviour
+{
+    public float BobbingMagnitude = 10;
+    public float BobbingSeed = 1;
+    public bool MoveX = true;
+    public bool MoveY = true;
+
+    Vector2 startPos;
+    Vector3 startScale;
+
+    void Start()
+    {
+        startPos = transform.localPosition;
+        startScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = new Vector2(
+            MoveX ? startPos.x + BobbingMagnitude * (Mathf.Sin(Time.time * BobbingSeed)) : startPos.x,
+            MoveY ? startPos.y + BobbingMagnitude * (Mathf.Sin(Time.time * BobbingSeed)) : startPos.y
+        );
+
+        transform.localScale = new Vector3(
+            startScale.x + .1f * (Mathf.Sin(Time.time * BobbingSeed)),
+            startScale.y + .1f * (Mathf.Sin(Time.time * BobbingSeed)),
+            startScale.z + .1f * (Mathf.Sin(Time.time * BobbingSeed))
+        );
+    }
+}
